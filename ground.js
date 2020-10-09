3ccd72be82aaa380c9a0c780e30886a131190031
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-09 11:28:17 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class Ground{
+    constructor(x,y){
+      
+      var options={
+        isStatic:true
+      }
+        
+      this.body=Bodies.rectangle(x,y,800,20,options);
+      
+      this.body.width=800;
+      this.body.height=20;
+      World.add(world,this.body);
+    }
+    display(){
+        var pos =this.body.position;
+    
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill(0,255,0);
+        rect(0, 0, 800,20);
+        pop();
+    }
+  }
