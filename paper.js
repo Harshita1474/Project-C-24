1463354dd0603ee50323948b53c792bc27063261
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-10 12:06:40 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,9 +1,14 @@
 class Paper{
     constructor(x,y){
       
+        var options={
+          isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
         
-        
-      this.body=Bodies.circle(x,y,20,20);
+      this.body=Bodies.circle(x,y,20,20,options);
       
       this.body.width=20;
       this.body.height=20;
