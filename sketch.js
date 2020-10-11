6ba4d0b79f56f61d65fc4f64a4e8adb278b4514d
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-11 12:23:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var paperObject,ground;
+var paper,ground;
 var dustbinSide1,dustbinSide2,dustbinSide3;
 
 function preload()
@@ -23,14 +24,14 @@ function setup() {
 
 	
 
-	paperObject=new Paper(100,650,width,height);
-
+	
 	dustbinSide1=new Dustbin(500,610,20,100);
 	dustbinSide2=new Dustbin(700,610,20,100);
 
 	dustbinSide3=createSprite(600,650,200,20);
 	dustbinSide3.shapeColor=color(255);
     ground=new Ground(400,670,800,20);
+    paperObject=new Paper(100,650,width,height);
 
 
 
@@ -49,7 +50,7 @@ function draw() {
   ground.display();}
 
 function keyPressed(){
-	if(keyCode === UP_ARROW){
-		Matter.Body.applyforce(paperObject.body,paperObject.body.position,{x:85,y:-85})
-	}
+if(keyCode === UP_ARROW){
+	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+}
 }
